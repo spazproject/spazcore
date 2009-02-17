4576530108dd9e2b224718931343a83197053c97
Ed Finkler <funkatron@gmail.com>
2009-02-17 13:41:42 -0500

    added skeleton for spazcron

---
+++ libs/spazcron.js
@@ -0,0 +1,23 @@
+
+/**
+ * SpazCron handles repetitive tasks in a somewhat cron-like way. 
+ * @param {integer} interval the interval to check for executable jobs IN SECONDS
+ */
+SpazCron = function(interval) {
+	if (!interval) {
+		this.interval = 15*1000; // 15 seconds
+	} else {
+		this.interval = interval*1000;
+	}
+	
+	this._jobs = {}
+		
+};
+
+SpazCron.prototype.addJob = function(name, function, mintime) {};
+
+
+
+SpazCronJob = function(name, function, mintime) {
+	
+};
