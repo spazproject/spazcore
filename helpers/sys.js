cfed9609188b8a3d46129a345c13da54d8cf9480
Ed Finkler <funkatron@gmail.com>
2009-04-22 10:22:41 -0400

    - removed old, extra jQuery versions - broke out the platform-specific stuff in helpers/sys.js under the new /platforms/ folder. To use platform-specific definitions, you will need to load the appropriate platform file(s) - added a very incomplete skeleton for spazpingfm.js - made note of need and stub files to break out platform-specific calls in spazprefs.js

--- helpers/sys.js
+++ helpers/sys.js
@@ -1,7 +1,9 @@
-/*
-	These are system-oriented functions, mostly utilizing AIR apis
-	to interact with the OS
-*/
+/**
+ * These are system-oriented functions, mostly utilizing AIR apis
+ * to interact with the OS
+ * 
+ * NOTE: to use all these helpers, you must additionally load a platform-specific definition file!
+ */
 
 
 const SPAZCORE_PLATFORM_AIR			= 'AIR';
@@ -11,7 +13,7 @@ const SPAZCORE_PLATFORM_UNKNOWN		= '__UNKNOWN';
 
 
 /**
-* returns a string identifier for the platform
+* Returns a string identifier for the platform.
 * 
 * Right now these checks are really, really basic
 * 
@@ -61,264 +63,122 @@ sc.helpers.isTitanium = function() {
 }
 
 
-/*
-	dump an object's first level to console
-*/
+/**
+ * A simple logging function
+ * @platformstub
+ */
 sc.helpers.dump = function(obj) {
-
-	
-	var dumper = function(obj) {
-		return false;
-	};
-	
-	/*
-		for AIR
-	*/
-	if (sc.helpers.isAIR()) {
-		dumper = function() {
-			for(var x in obj) {
-				air.trace("'"+x+"':"+obj[x]);
-			}
-		}
-	}
-	
-	/*
-		for Mojo
-	*/
-	if (sc.helpers.iswebOS()) {
-		if (sc.helpers.isString(obj) || sc.helpers.isNumber(obj) || !obj) {
-			dumper = Mojo.Log.info;
-		} else {
-			dumper = Mojo.Log.logProperties;
-		}
-		
-	}
-	
-	/*
-	* for Titanium
-	* 
-	* @todo this needs to be tested
-	*
-	*/
-	if (sc.helpers.isTitanium()) {
-		if (sc.helpers.isString(obj) || sc.helpers.isNumber(obj) || !obj) {
-			dumper = function(str) {
-				console.debug(str);
-			}
-		} else {
-			dumper = function(obj) {
-				console.dir(obj);
-			}
-		}
-	}
-	
-	
-	if (sc.helpers.isString(obj)) {
-		dumper(obj);
-	} else if(sc.helpers.isNumber(obj)) {
-		dumper(obj.toString());
-	} else if (obj === undefined) {
-		dumper('UNDEFINED');
-	} else if (obj === null) {
-		dumper('NULL');
-	} else { // this is an object. we hope.
-		if (dump) { // we really prefer to use dump if it is available
-			dumper(obj);
-		} else {
-			dumper(obj)
-		}
-		
-	}
-
-	
+	// stub
 }
 
-/*
-	Open a URL in the default system web browser
-*/
+/**
+ * Open a URL in the default system web browser
+ * @platformstub
+ */
 sc.helpers.openInBrowser = function(url) {
-	var request = new air.URLRequest(url);
-	try {            
-	    air.navigateToURL(request);
-	} catch (e) {
-	    air.trace(e.errorMsg);
-		dump(e.name + ":" + e.message);
-	}
+	// stub
 }
 
-/*
-	Gets the contents of a file
-*/
+/**
+ * Gets the contents of a file
+ * @platformstub
+ */
 sc.helpers.getFileContents = function(path) {
-	var f = new air.File(path);
-	if (f.exists) {
-		var fs = new air.FileStream();
-		fs.open(f, air.FileMode.READ);
-		var str = fs.readMultiByte(f.size, air.File.systemCharset);
-		fs.close();
-		return str;
-	} else {
-		return false;
-	}
+	// stub
 }
 
-/*
-	Saves the contents to a specified path. Serializes a passed object if 
-	serialize == true
-*/
+/**
+ * Saves the contents to a specified path. Serializes a passed object if 
+ * serialize == true
+ * @platformstub
+ */
 sc.helpers.setFileContents = function(path, content, serialize) {
-	
-	if (serialize) {
-		content = JSON.stringify(content);
-	}
-	
-	Spaz.dump('setFileContents for '+path+ ' to "' +content+ '"');
-	
-	try { 
-		var f = new air.File(path);
-		var fs = new air.FileStream();
-		fs.open(f, air.FileMode.WRITE);
-		fs.writeUTFBytes(content);
-		fs.close();
-	} catch (e) {
-		air.trace(e.errorMsg)
-		dump(e.name + ":" + e.message);
-	}
+	// stub
 };
 
 
-/*
-	Returns the current application version string
-*/
+/**
+ * Returns the current application version string
+ * @platformstub
+ */
 sc.helpers.getAppVersion = function() {
-	var appXML = air.NativeApplication.nativeApplication.applicationDescriptor
-	var domParser = new DOMParser();
-	appXML = domParser.parseFromString(appXML, "text/xml");
-	var version = appXML.getElementsByTagName("version")[0].firstChild.nodeValue;
-	return version;
+	// stub
 };
 
 
-/*
-	Returns the user agent string for the app
-*/
+/**
+ * Returns the user agent string for the app
+ * @platformstub
+ */
 sc.helpers.getUserAgent = function() {
-	return window.htmlLoader.userAgent
+	// stub
 };
 
-/*
-	Sets the user agent string for the app
-*/
+/**
+ * Sets the user agent string for the app
+ * @platformstub
+ */
 sc.helpers.setUserAgent = function(uastring) {
-	window.htmlLoader.userAgent = uastring
-	// air.URLRequestDefaults.userAgent = uastring
-	return window.htmlLoader.userAgent
+	// stub
 };
 
-/*
-	Gets clipboard text
-*/
+/**
+ * Gets clipboard text
+ * @platformstub
+ */
 sc.helpers.getClipboardText = function() {
-	if(air.Clipboard.generalClipboard.hasFormat("text/plain")){
-	    var text = air.Clipboard.generalClipboard.getData("text/plain");
-		return text;
-	} else {
-		return '';
-	}
+	// stub
 }
 
-/*
-	Sets clipboard text
-*/
+/**
+ * Sets clipboard text
+ * @platformstub
+ */
 sc.helpers.setClipboardText = function(text) {
-	Spaz.dump('Copying "' + text + '" to clipboard');
-	air.Clipboard.generalClipboard.clear();
-	air.Clipboard.generalClipboard.setData(air.ClipboardFormats.TEXT_FORMAT,text,false);
+	// stub
 }
 
 
-/*
-	Loads a value for a key from EncryptedLocalStore
-*/
+/**
+ * Loads a value for a key from EncryptedLocalStore
+ * @platformstub
+ */
 sc.helpers.getEncryptedValue = function(key) {
-	var storedValue = air.EncryptedLocalStore.getItem(key);
-	var val = storedValue.readUTFBytes(storedValue.length);
-	return val;
+	// stub
 }
 
-/*
-	Sets a value in the EncryptedLocalStore of AIR
-*/
+/**
+ * Sets a value in the EncryptedLocalStore of AIR
+ * @platformstub
+ */
 sc.helpers.setEncyrptedValue = function(key, val) {
-	var bytes = new air.ByteArray();
-    bytes.writeUTFBytes(val);
-    air.EncryptedLocalStore.setItem(key, bytes);
+	// stub
 }
 
 
-/*
-	Get the app storage directory
-*/
+/**
+ * Get the app storage directory
+ * @TODO is there an equivalent for this on all platforms?
+ * @platformstub
+ */
 sc.helpers.getAppStoreDir = function() {
-	return air.File.applicationStorageDirectory;
+	// stub
 }
 
-
+/**
+ * Get the preferences file
+ * @TODO this should be removed and we rely on the preferences lib 
+ */
 sc.helpers.getPreferencesFile = function(name, create) {
-	if (!name) {name='preferences';}
-	
-	var prefsFile = getAppStoreDir();
-	prefsFile = prefsFile.resolvePath(name+".json");
-	
-	// if 
-	
-	return prefsFile;
+	// stub
 }
 
-/*
-	initializes a file at the given location. set overwrite to true
-	to clear out an existing file.
-	returns the air.File object or false
+/**
+ * initializes a file at the given location. set overwrite to true
+ * to clear out an existing file.
+ * returns the air.File object or false
+ * @platformstub
 */
 sc.helpers.init_file = function(path, overwrite) {
-	var file = new air.File(path);
-	if ( !file.exists || (file.exists && overwrite) ) {
-		var fs = new air.FileStream();
-		fs.open(file, air.FileMode.WRITE);
-		fs.writeUTFBytes('');
-		fs.close();
-		return file;
-	} else {
-		return false;
-	}
-	
+	// stub
 }
-
-
-
-
-
-
-
-
-
-// var fs = new air.FileStream();
-// 
-// if (prefsFile.exists) {
-//     fs.open(prefsFile, air.FileMode.READ);
-//     var prefsJSON = fs.readUTFBytes(prefsFile.size);
-//     var loadedpreferences = JSON.parse(prefsJSON);
-// 
-//     for (key in loadedpreferences) {
-// 
-//         Spaz.Prefs.preferences[key] = loadedpreferences[key];
-//         if (Spaz.Prefs.changeMethods[key] && Spaz.Prefs.changeMethods[key].check) {
-//             Spaz.Prefs.changeMethods[key].check();
-//         }
-//     }
-// } else {
-//     fs.open(prefsFile, air.FileMode.WRITE);
-//     fs.writeUTFBytes(JSON.stringify(Spaz.Prefs.defaultPreferences));
-//     Spaz.Prefs.preferences = clone(Spaz.Prefs.defaultPreferences);
-// }
-// fs.close()
---
+++ platforms/AIR/helpers/sys.js
@@ -0,0 +1,193 @@
+/*
+	We load this file to redefine platform-specific methods
+*/
+
+/*
+	dump an object's first level to console
+*/
+sc.helpers.dump = function(obj) {
+	
+	if (sc.helpers.isString(obj) || sc.helpers.isNumber(obj) || !obj) {
+		var dumper = air.trace;
+	} else {
+		var dumper = function() {
+			for(var x in obj) {
+				air.trace("'"+x+"':"+obj[x]);
+			}
+		}
+	}
+	
+	if (sc.helpers.isString(obj)) {
+		dumper(obj);
+	} else if(sc.helpers.isNumber(obj)) {
+		dumper(obj.toString());
+	} else if (obj === undefined) {
+		dumper('UNDEFINED');
+	} else if (obj === null) {
+		dumper('NULL');
+	} else { // this should be a "normal" object
+		dumper(obj)
+	}
+}
+
+
+/*
+	Open a URL in the default system web browser
+*/
+sc.helpers.openInBrowser = function(url) {
+	var request = new air.URLRequest(url);
+	try {            
+	    air.navigateToURL(request);
+	} catch (e) {
+	    air.trace(e.errorMsg);
+	}
+}
+
+
+/*
+	Gets the contents of a file
+*/
+sc.helpers.getFileContents = function(path) {
+	var f = new air.File(path);
+	if (f.exists) {
+		var fs = new air.FileStream();
+		fs.open(f, air.FileMode.READ);
+		var str = fs.readMultiByte(f.size, air.File.systemCharset);
+		fs.close();
+		return str;
+	} else {
+		return false;
+	}
+}
+
+
+sc.helpers.setFileContents = function(path, content, serialize) {
+	
+	if (serialize) {
+		content = JSON.stringify(content);
+	}
+	
+	Spaz.dump('setFileContents for '+path+ ' to "' +content+ '"');
+	
+	try { 
+		var f = new air.File(path);
+		var fs = new air.FileStream();
+		fs.open(f, air.FileMode.WRITE);
+		fs.writeUTFBytes(content);
+		fs.close();
+	} catch (e) {
+		air.trace(e.errorMsg)
+		dump(e.name + ":" + e.message);
+	}
+};
+
+
+/*
+	Returns the current application version string
+*/
+sc.helpers.getAppVersion = function() {
+	var appXML = air.NativeApplication.nativeApplication.applicationDescriptor
+	var domParser = new DOMParser();
+	appXML = domParser.parseFromString(appXML, "text/xml");
+	var version = appXML.getElementsByTagName("version")[0].firstChild.nodeValue;
+	return version;
+};
+
+
+/*
+	Returns the user agent string for the app
+*/
+sc.helpers.getUserAgent = function() {
+	return window.htmlLoader.userAgent;
+};
+
+
+/*
+	Sets the user agent string for the app
+*/
+sc.helpers.setUserAgent = function(uastring) {
+	window.htmlLoader.userAgent = uastring;
+	return window.htmlLoader.userAgent;
+};
+
+
+
+/*
+	Gets clipboard text
+*/
+sc.helpers.getClipboardText = function() {
+	if(air.Clipboard.generalClipboard.hasFormat("text/plain")){
+	    var text = air.Clipboard.generalClipboard.getData("text/plain");
+		return text;
+	} else {
+		return '';
+	}
+}
+
+/*
+	Sets clipboard text
+*/
+sc.helpers.setClipboardText = function(text) {
+	Spaz.dump('Copying "' + text + '" to clipboard');
+	air.Clipboard.generalClipboard.clear();
+	air.Clipboard.generalClipboard.setData(air.ClipboardFormats.TEXT_FORMAT,text,false);
+}
+
+
+/*
+	Loads a value for a key from EncryptedLocalStore
+*/
+sc.helpers.getEncryptedValue = function(key) {
+	var storedValue = air.EncryptedLocalStore.getItem(key);
+	var val = storedValue.readUTFBytes(storedValue.length);
+	return val;
+}
+
+/*
+	Sets a value in the EncryptedLocalStore of AIR
+*/
+sc.helpers.setEncyrptedValue = function(key, val) {
+	var bytes = new air.ByteArray();
+    bytes.writeUTFBytes(val);
+    air.EncryptedLocalStore.setItem(key, bytes);
+}
+
+
+/*
+	Get the app storage directory
+*/
+sc.helpers.getAppStoreDir = function() {
+	return air.File.applicationStorageDirectory;
+}
+
+
+/**
+ * 
+ */
+sc.helpers.getPreferencesFile = function(name, create) {
+	if (!name) {name='preferences';}
+	
+	var prefsFile = getAppStoreDir();
+	prefsFile = prefsFile.resolvePath(name+".json");
+	
+	return prefsFile;
+}
+
+/*
+	initializes a file at the given location. set overwrite to true
+	to clear out an existing file.
+	returns the air.File object or false
+*/
+sc.helpers.init_file = function(path, overwrite) {
+	var file = new air.File(path);
+	if ( !file.exists || (file.exists && overwrite) ) {
+		var fs = new air.FileStream();
+		fs.open(file, air.FileMode.WRITE);
+		fs.writeUTFBytes('');
+		fs.close();
+		return file;
+	} else {
+		return false;
+	}
+
+}
