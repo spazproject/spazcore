2993fa7da8dfda87244b0a17af6dd3b9210b6576
Ed Finkler <funkatron@gmail.com>
2009-08-13 15:36:59 -0400

    - made new date.js file that is not minified - minor spazprefs.js update

---
+++ bin/build-datejs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# this is really more here for reference so we rememeber how to build a working date.js
+cat globalization/en-US.js core.js parser.js > ~/Sites/spazcore/vendors/date.js
