140007ba548cc004d6f312115fd387a4777887bb
Ed Finkler <funkatron@gmail.com>
2010-01-11 21:01:27 -0500

    - added new placeholders for spazprefs.js platform-specific stuff - about 400 new entries for spazshorttext.js - lots of improvements to spazaccounts.js - account management example

--- platforms/AIR/libs/spazprefs.js
+++ platforms/AIR/libs/spazprefs.js
@@ -10,5 +10,36 @@ onevar: false
 var sc, air;
 
 /**
+ * AIR
  * platform-specific definitions for prefs lib 
  */
+
+SpazPrefs.prototype.load = function() {
+	
+}
+
+SpazPrefs.prototype.save = function() {
+	
+};
+
+
+SpazPrefs.prototype.getEncrypted = function(key) {
+	
+};
+
+
+SpazPrefs.prototype.setEncrypted = function(key, val) {
+	
+};
+
+
+
+SpazPrefs.prototype.saveWindowState = function() {
+	
+};
+
+
+SpazPrefs.prototype.loadWindowState = function() {
+	
+};
+
--- platforms/Titanium/libs/spazprefs.js
+++ platforms/Titanium/libs/spazprefs.js
@@ -1,3 +1,44 @@
+/*jslint 
+browser: true,
+nomen: false,
+debug: true,
+forin: true,
+undef: true,
+white: false,
+onevar: false 
+ */
+var sc, Titanium;
+ 
 /**
+ * TITANIUM
  * platform-specific definitions for prefs lib 
  */
+
+SpazPrefs.prototype.load = function() {
+
+}
+
+SpazPrefs.prototype.save = function() {
+
+};
+
+
+SpazPrefs.prototype.getEncrypted = function(key) {
+
+};
+
+
+SpazPrefs.prototype.setEncrypted = function(key, val) {
+
+};
+
+
+
+SpazPrefs.prototype.saveWindowState = function() {
+
+};
+
+
+SpazPrefs.prototype.loadWindowState = function() {
+
+};
