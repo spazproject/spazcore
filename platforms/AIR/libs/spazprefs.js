119801f4d6f3080bdc1a5b35e0e57e1e47f50791
Ed Finkler <funkatron@gmail.com>
2010-01-19 14:01:23 -0500

    refactored a lot of prefs stuff to use separate platform files instead of lame platform checking inside the lib itself

--- platforms/AIR/libs/spazprefs.js
+++ platforms/AIR/libs/spazprefs.js
@@ -15,31 +15,82 @@ var sc, air;
  */
 
 SpazPrefs.prototype.load = function() {
+	var filename = this.id || SPAZCORE_PREFS_AIR_FILENAME;
 	
+	var prefsFile = air.File.applicationStorageDirectory;
+    prefsFile = prefsFile.resolvePath(filename);
+
+    var fs = new air.FileStream();
+
+    if (prefsFile.exists) {
+		sch.debug('prefsfile exists');
+        fs.open(prefsFile, air.FileMode.READ);
+        var prefsJSON = fs.readUTFBytes(prefsFile.size);
+        sch.debug(prefsJSON)
+        var loaded_prefs = JSON.parse(prefsJSON);
+
+		for (var key in loaded_prefs) {
+			sc.helpers.dump('Copying loaded pref "' + key + '":"' + this._prefs[key] + '" (' + typeof(this._prefs[key]) + ')');
+            this._prefs[key] = loaded_prefs[key];
+       	}
+    } else { // init the file
+		sch.debug('prefs file does not exist; saving with defaults');
+        this.save();
+    }
+    fs.close()
 }
 
 SpazPrefs.prototype.save = function() {
-	
+	var jsonPrefs = sch.enJSON(Spaz.Prefs.preferences);
+    sch.debug(jsonPrefs);
+
+	var filename = this.id || SPAZCORE_PREFS_AIR_FILENAME;
+
+    var prefsFile = air.File.applicationStorageDirectory;
+    prefsFile = prefsFile.resolvePath(filename);
+
+    var fs = new air.FileStream();
+
+    fs.open(prefsFile, air.FileMode.WRITE);
+    fs.writeUTFBytes(sc.helpers.enJSON(this._prefs));
+    fs.close();
 };
 
 
 SpazPrefs.prototype.getEncrypted = function(key) {
-	
+	var storedValue = air.EncryptedLocalStore.getItem(key);
+	var val = storedValue.readUTFBytes(storedValue.length);
+	return val;
 };
 
 
 SpazPrefs.prototype.setEncrypted = function(key, val) {
-	
+	var bytes = new air.ByteArray();
+	bytes.writeUTFBytes(val);
+	return air.EncryptedLocalStore.setItem(key, bytes);
 };
 
 
 
 SpazPrefs.prototype.saveWindowState = function() {
-	
+	this.set('__window-height', window.nativeWindow.width);
+	this.set('__window-height', window.nativeWindow.height);
+	this.set('__window-x', window.nativeWindow.x);
+	this.set('__window-y', window.nativeWindow.y);
 };
 
 
 SpazPrefs.prototype.loadWindowState = function() {
+	var width  = this.get('__window-height');
+	var height = this.get('__window-height');
+	var x      = this.get('__window-x');
+	var y      = this.get('__window-y');
 	
+	if (x && y && width && height) {
+		window.nativeWindow.width  = width;
+		window.nativeWindow.height = height;
+		window.nativeWindow.x = x;
+		window.nativeWindow.y = y;
+	}
 };
 
--- platforms/Titanium/libs/spazprefs.js
+++ platforms/Titanium/libs/spazprefs.js
@@ -15,30 +15,57 @@ var sc, Titanium;
  */
 
 SpazPrefs.prototype.load = function() {
-
+	
+	var thisPrefs = this;
+	
+	
+	if (Titanium.App.Properties.hasProperty(SPAZCORE_PREFS_TI_KEY)) {
+		var prefs_json = Titanium.App.Properties.getString(SPAZCORE_PREFS_TI_KEY);
+		var loaded_prefs = sc.helpers.deJSON(prefs_json);
+		for (var key in loaded_prefs) {
+			sc.helpers.dump('Copying loaded pref "' + key + '":"' + this._prefs[key] + '" (' + typeof(this._prefs[key]) + ')');
+            this._prefs[key] = loaded_prefs[key];
+       	}
+	} else {
+		// save the defaults if this is the first time
+		this.save();
+	}
+	// jQuery().trigger('spazprefs_loaded');
 }
 
 SpazPrefs.prototype.save = function() {
-
+	// save the file to a default place
+	var prefs_json = sc.helpers.enJSON(this._prefs);
+	Titanium.App.Properties.setString(SPAZCORE_PREFS_TI_KEY, prefs_json);
 };
 
 
+/**
+ * @todo 
+ */
 SpazPrefs.prototype.getEncrypted = function(key) {
-
+	
 };
 
-
+/**
+ * @todo 
+ */
 SpazPrefs.prototype.setEncrypted = function(key, val) {
 
 };
 
 
-
+/**
+ * @todo 
+ */
 SpazPrefs.prototype.saveWindowState = function() {
 
 };
 
 
+/**
+ * @todo 
+ */
 SpazPrefs.prototype.loadWindowState = function() {
 
 };
