2304290f8ce31665beae748bb274a8f849528933
Ben Ramsey <ramsey@php.net>
2010-02-28 22:14:36 -0600

    Finished OAuth example and refactored all code into the SpazOAuth class.

--- incubator/libs/spazoauth.js
+++ incubator/libs/spazoauth.js
@@ -4,9 +4,12 @@
  *
  * @param {object} (service string)
  */
-function SpazOAuthConsumer(args) {
+function SpazOAuth(args) {
 
 	this.services = {};
+	this.requestToken = null;
+	this.accessToken = null;
+	this.accessTokenSecret = null;
 
 	this.initServices();
 	this.setService(args.service);
@@ -16,7 +19,7 @@ function SpazOAuthConsumer(args) {
 /**
  * Initialize OAuth services that Spaz can use
  */
-SpazOAuthConsumer.prototype.initServices = function() {
+SpazOAuth.prototype.initServices = function() {
 	this.addService('twitter', {
 		signatureMethod      : 'HMAC-SHA1',
 		consumerKey          : 'jZZNzafe1ixepaFWrnkBg',
@@ -29,13 +32,13 @@ SpazOAuthConsumer.prototype.initServices = function() {
 
 /**
  * Adds a new OAuth service
- * @param {string} service_name
+ * @param {string} serviceName
  * @param {object} opts (signatureMethod string, consumerKey string, consumerSecret string, requestTokenUrl string, accessTokenUrl string, userAuthorizationUrl string)
  */
-SpazOAuthConsumer.prototype.addService = function(service_name, opts) {
+SpazOAuth.prototype.addService = function(serviceName, opts) {
 
 	var service = {};
-	service.name                 = service_name;
+	service.name                 = serviceName;
 	service.signatureMethod      = opts.signatureMethod;
 	service.consumerKey          = opts.consumerKey;
 	service.consumerSecret       = opts.consumerSecret;
@@ -43,22 +46,155 @@ SpazOAuthConsumer.prototype.addService = function(service_name, opts) {
 	service.accessTokenUrl       = opts.accessTokenUrl;
 	service.userAuthorizationUrl = opts.userAuthorizationUrl;
 
-	this.services[service_name] = service;
+	this.services[serviceName] = service;
 
 };
 
 /**
  * Sets the service currently being used by Spaz
- * @param {string} service_name
+ * @param {string} serviceName
  */
-SpazOAuthConsumer.prototype.setService = function(service_name) {
-	this.service = service_name;
+SpazOAuth.prototype.setService = function(serviceName) {
+	this.service = serviceName;
 };
 
 /**
  * Returns the service set on object instantiation
  */
-SpazOAuthConsumer.prototype.getService = function() {
+SpazOAuth.prototype.getService = function() {
 	return this.services[this.service];
+};
+
+/**
+ * Gets the request token
+ */
+SpazOAuth.prototype.getRequestToken = function() {
+
+	var success = false;
+	var requestToken = null;
+	var userAuthorizationUrl = this.getService().userAuthorizationUrl;
+	var method = 'post';
+	var authHeader = this.getAuthHeader({
+		method: method,
+		url: this.getService().requestTokenUrl
+	});
+
+	$.ajax({
+		async: false,
+		type: method,
+		url: this.getService().requestTokenUrl,
+		beforeSend: function(req) {
+			req.setRequestHeader('Authorization', authHeader);
+			req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		},
+		complete: function(req, textStatus) {
+			if (req.status == 200) {
+				var results = OAuth.decodeForm(req.responseText);
+				requestToken = OAuth.getParameter(results, 'oauth_token');
+
+				// Open another browser window to allow the user to authorize
+				// service access to Spaz and retrieve the authorization PIN
+				sc.helpers.openInBrowser(OAuth.addToURL(
+					userAuthorizationUrl,
+					{oauth_token : requestToken}
+				));
+
+				success = true;
+			}
+		},
+	});
+
+	if (success) {
+		this.requestToken = requestToken;
+	}
+
+	return success;
+
+};
+
+/**
+ * Authorize the user with the service and store the access token locally
+ * @param {string} accessPIN
+ */
+SpazOAuth.prototype.getAuthorization = function(accessPIN) {
+
+	if (!accessPIN || !this.requestToken) {
+		return false;
+	}
+
+	var success = false;
+	var accessToken = null;
+	var accessTokenSecret = null;
+	var method = 'post';
+	var authHeader = this.getAuthHeader({
+		method: method,
+		url: this.getService().accessTokenUrl,
+		parameters: [
+			['oauth_verifier', accessPIN],
+			['oauth_token', this.requestToken]
+		]
+	});
+
+	$.ajax({
+		async: false,
+		type: method,
+		url: this.getService().accessTokenUrl,
+		beforeSend: function(req) {
+			req.setRequestHeader('Authorization', authHeader);
+			req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		},
+		complete: function(req, textStatus) {
+			if (req.status == 200) {
+				var results = OAuth.decodeForm(req.responseText);
+				accessToken = OAuth.getParameter(results, 'oauth_token');
+				accessTokenSecret = OAuth.getParameter(results, 'oauth_token_secret');
+
+				success = true;
+			}
+		},
+	});
+
+	if (success) {
+		this.accessToken = accessToken;
+		this.accessTokenSecret = accessTokenSecret;
+	}
+
+	return success;
+};
+
+/**
+ * Returns the value of the HTTP authorization header to use for the request
+ * @param {array} params The OAuth params to use when creating the auth header
+ * @param {array} options Must include method and url, optionally parameters
+ */
+SpazOAuth.prototype.getAuthHeader = function(options) {
+
+	if (!options.method || !options.url) {
+		return false;
+	}
+
+	if (!options.parameters) {
+		options.parameters = [];
+	}
+
+	var message = {
+		method: options.method,
+		action: options.url,
+		parameters: options.parameters
+	};
+
+	OAuth.completeRequest(message, {
+		consumerKey: this.getService().consumerKey,
+		consumerSecret: this.getService().consumerSecret,
+		token: this.accessToken,
+		tokenSecret: this.accessTokenSecret
+	});
+
+	var authHeader = OAuth.getAuthorizationHeader(
+		this.getService().name,
+		message.parameters
+	);
+
+	return authHeader;
 };
 
