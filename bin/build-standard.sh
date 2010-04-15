4b1beb4dada11804c400e2f018261e291a7906ee
Ed Finkler <funkatron@gmail.com>
2010-04-15 19:07:39 -0400

    added minimization with YUIcompressor.jar

--- bin/build-standard.sh
+++ bin/build-standard.sh
@@ -2,6 +2,7 @@
 # run this from the base dir of SpazCore
 
 BUILD_FILE='builds/spazcore-standard.js'
+MIN_FILE='builds/spazcore-standard.min.js'
 BUILD_DATE=`date "+%Y-%m-%d %H:%M:%S %Z"`
 
 echo "/*********** Built ${BUILD_DATE} ***********/" > $BUILD_FILE
@@ -12,7 +13,6 @@ cat libs/spazcore.js \
 	vendors/json2.js \
 	vendors/sizzle.js \
 	vendors/underscore.js \
-	vendors/shortcut.js \
 	vendors/shortcut.js \
 	vendors/jquery.cookies.2.2.0.js \
 	helpers/datetime.js \
@@ -40,3 +40,5 @@ cat libs/spazcore.js \
 	libs/spaztwit.js \
 	platforms/standard/libs/spazprefs.js \
 	>> $BUILD_FILE
+
+java -jar ~/Library/Application\ Support/TextMate/Bundles/javascript-tools.tmbundle/Support/bin/yuicompressor.jar --charset utf8 --preserve-semi $BUILD_FILE -o $MIN_FILE
