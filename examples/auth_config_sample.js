eb1021e2b1000cfbb800fe971fcf165e9e9f9f9c
Ed Finkler <funkatron@gmail.com>
2010-06-13 02:21:56 -0400

    updated test_account_flow.html and added auth_config_sample.js

---
+++ examples/auth_config_sample.js
@@ -0,0 +1,7 @@
+/*
+	Rename this file to auth_config.js 
+	and fill in the values below with your own
+	xAuth-approved key and secret
+*/
+var SPAZCORE_CONSUMERKEY_TWITTER = '';
+var SPAZCORE_CONSUMERSECRET_TWITTER = '';
