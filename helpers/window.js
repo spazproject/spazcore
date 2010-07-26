4a0bea0a85a726726f5c4728c20d6410c3f1c123
Ed Finkler <coj@funkatron.com>
2010-07-26 15:16:03 -0400

    Initial stubs for window helpers

---
+++ helpers/window.js
@@ -0,0 +1,24 @@
+/*jslint 
+browser: true,
+nomen: false,
+debug: true,
+forin: true,
+undef: true,
+white: false,
+onevar: false 
+ */
+var sc, DOMParser;
+
+sc.helpers.openWindow = function(opts) {};
+
+
+sc.helpers.closeWindow = function(opts) {};
+
+
+sc.helpers.activateWindow = function(opts) {};
+
+
+sc.helpers.getCurrentWindow = function(opts) {};
+
+
+sch.helpers.getOpenWindows = function(opts) {};
