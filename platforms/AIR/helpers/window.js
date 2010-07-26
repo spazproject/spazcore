ec3074638e602b96047ce6cc1982dcd3bc56b636
Ed Finkler <coj@funkatron.com>
2010-07-26 15:17:07 -0400

    initial platform stubs for window helpers

---
+++ platforms/AIR/helpers/window.js
@@ -0,0 +1,38 @@
+/*jslint 
+browser: true,
+nomen: false,
+debug: true,
+forin: true,
+undef: true,
+white: false,
+onevar: false 
+ */
+var sc, DOMParser;
+
+/*
+    AIR implementation of window helpers
+*/
+
+
+sc.helpers.openWindow = function(opts) {
+    
+    // systemChrome = NativeWindowSystemChrome.STANDARD
+    // type = NativeWindowType.NORMAL
+    // transparent = false
+    // resizable = true
+    // maximizable = true
+    // minimizable = true
+    
+};
+
+
+sc.helpers.closeWindow = function(opts) {};
+
+
+sc.helpers.activateWindow = function(opts) {};
+
+
+sc.helpers.getCurrentWindow = function(opts) {};
+
+
+sch.helpers.getOpenWindows = function(opts) {};
---
+++ platforms/Titanium/helpers/window.js
@@ -0,0 +1,29 @@
+/*jslint 
+browser: true,
+nomen: false,
+debug: true,
+forin: true,
+undef: true,
+white: false,
+onevar: false 
+ */
+var sc, DOMParser;
+
+/*
+    Titanium Desktop implementation of window helpers
+*/
+
+
+sc.helpers.openWindow = function(opts) {};
+
+
+sc.helpers.closeWindow = function(opts) {};
+
+
+sc.helpers.activateWindow = function(opts) {};
+
+
+sc.helpers.getCurrentWindow = function(opts) {};
+
+
+sch.helpers.getOpenWindows = function(opts) {};
